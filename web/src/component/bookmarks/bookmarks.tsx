cd69da59a3acd256ba350d61daaf6742b049ca9a
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2022-09-04 22:36:14 -0700

    bookmark table components

--- web/src/component/bookmarks/bookmarks.tsx
+++ web/src/component/bookmarks/bookmarks.tsx
@@ -1,58 +1,37 @@
+import "./style.css";
 import React from "react";
 import { Bookmark as BookmarkType } from "../../../../graphql/genql/schema";
 import { useNavigate } from "react-router-dom";
+import { TableContainer, Table, Tbody, Tr, Td } from "@chakra-ui/react";
 
 export const Bookmarks: React.FC<{
   bookmarks: BookmarkType[] | null;
 }> = (props) => {
-  if (props.bookmarks === null) {
-    return (
-      <div>
-        <div>loading...</div>
-      </div>
-    );
-  }
-
-  if (props.bookmarks.length < 1) {
-    return (
-      <div>
-        <div>no bookmarks</div>
-      </div>
-    );
-  }
-
-  return (
-    <div>
-      {props.bookmarks.map((e) => (
-        <Bookmark key={e.bookmarkId} bookmark={e} />
-      ))}
-    </div>
-  );
-};
-
-const Bookmark: React.FC<{
-  bookmark: BookmarkType;
-}> = (props) => {
   const navigate = useNavigate();
 
   return (
-    <div
-      key={props.bookmark.bookmarkId}
-      style={{
-        display: "flex",
-        justifyContent: "space-between",
-      }}
-    >
-      <div>{props.bookmark.title}</div>
-      <div>{props.bookmark.url}</div>
-
-      <button
-        onClick={() => {
-          navigate(`bookmark/${props.bookmark.bookmarkId}`);
-        }}
-      >
-        Details
-      </button>
+    <div>
+      <TableContainer>
+        <Table className="bookmarkTable">
+          <Tbody>
+            {props.bookmarks?.map((e) => (
+              <Tr key={e.bookmarkId}>
+                <Td>{e.title}</Td>
+                <Td>{e.url}</Td>
+                <Td width={"96px"}>
+                  <button
+                    onClick={() => {
+                      navigate(`bookmark/${e.bookmarkId}`);
+                    }}
+                  >
+                    Edit
+                  </button>
+                </Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </TableContainer>
     </div>
   );
 };
