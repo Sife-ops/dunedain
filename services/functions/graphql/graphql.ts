de4ea2a54193748bca65c4681aa5b20385181237
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2022-09-06 20:22:26 -0700

    remove console.log

--- services/functions/graphql/graphql.ts
+++ services/functions/graphql/graphql.ts
@@ -8,8 +8,6 @@ export const handler = createGQLHandler({
     const auth = request.event.headers.authorization?.split(' ')[1]
     const { username } = decode(auth!) as { username: string };
 
-    console.log(username);
-
     if (!username) {
       throw new Error('could not extract username from token');
     }
