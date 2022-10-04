bccf226af25421c47ac3a12e88622229152625f4
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2022-10-04 16:29:50 -0700

    create user

--- services/functions/auth/sign-up.ts
+++ services/functions/auth/sign-up.ts
@@ -1,3 +1,18 @@
-export const handler = () => {
-  console.log("hello");
+import { dunedainModel } from "@dunedain/core/model";
+import { z } from "zod";
+
+export const handler = async (event: any) => {
+  const eventSchema = z.object({
+    email: z.string(),
+    password: z.string(),
+  });
+
+  const { email, password } = eventSchema.parse(JSON.parse(event.body));
+
+  const res = await dunedainModel.entities.UserEntity.create({
+    email,
+    password,
+  }).go();
+
+  console.log(res)
 };
